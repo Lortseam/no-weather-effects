908d1defd7b664a9ee1588906c4e85925b066118
Lortseam <lortseam@gmail.com>
2021-02-14 14:40:41 +0100

    Initial commit

---
+++ src/main/java/me/lortseam/norain/mixin/ClientWorldMixin.java
@@ -0,0 +1,30 @@
+package me.lortseam.norain.mixin;
+
+import net.minecraft.client.world.ClientWorld;
+import net.minecraft.util.profiler.Profiler;
+import net.minecraft.util.registry.RegistryKey;
+import net.minecraft.world.MutableWorldProperties;
+import net.minecraft.world.World;
+import net.minecraft.world.dimension.DimensionType;
+import org.spongepowered.asm.mixin.Mixin;
+
+import java.util.function.Supplier;
+
+@Mixin(ClientWorld.class)
+public abstract class ClientWorldMixin extends World {
+
+    private ClientWorldMixin(MutableWorldProperties properties, RegistryKey<World> registryRef, DimensionType dimensionType, Supplier<Profiler> profiler, boolean isClient, boolean debugWorld, long seed) {
+        super(properties, registryRef, dimensionType, profiler, isClient, debugWorld, seed);
+    }
+
+    @Override
+    public float getRainGradient(float delta) {
+        return 0;
+    }
+
+    @Override
+    public float getThunderGradient(float delta) {
+        return 0;
+    }
+
+}
