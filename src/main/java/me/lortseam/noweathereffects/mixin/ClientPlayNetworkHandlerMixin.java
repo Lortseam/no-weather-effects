746b7cdf057f42aaca3344f13a43194c95d0657e
Lortseam <lortseam@gmail.com>
2021-06-08 22:29:48 +0200

    Minecraft 1.17 + Java 16

--- src/main/java/me/lortseam/noweathereffects/mixin/ClientPlayNetworkHandlerMixin.java
+++ src/main/java/me/lortseam/noweathereffects/mixin/ClientPlayNetworkHandlerMixin.java
@@ -1,6 +1,8 @@
 package me.lortseam.noweathereffects.mixin;
 
 import net.minecraft.client.network.ClientPlayNetworkHandler;
+import net.minecraft.entity.EntityType;
+import net.minecraft.network.packet.s2c.play.EntitySpawnS2CPacket;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -9,9 +11,11 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(ClientPlayNetworkHandler.class)
 public abstract class ClientPlayNetworkHandlerMixin {
 
-    @Inject(method = "onEntitySpawn", at = @At(value = "NEW", target = "net/minecraft/entity/LightningEntity"), cancellable = true)
-    private void noweathereffects$cancelLightningSpawn(CallbackInfo ci) {
-        ci.cancel();
+    @Inject(method = "onEntitySpawn", at = @At("HEAD"), cancellable = true)
+    private void noweathereffects$cancelLightningSpawn(EntitySpawnS2CPacket packet, CallbackInfo ci) {
+        if (packet.getEntityTypeId() == EntityType.LIGHTNING_BOLT) {
+            ci.cancel();
+        }
     }
 
 }
